94a66abda706bf1795f5beb7a6c94c9600650f64
Pep <salvadorpep@gmail.com>
2018-05-03 19:29:42 +0200

    Renaming folders

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import jinja2
 import webapp2
 
 
-template_dir = os.path.join(os.path.dirname(__file__), "template")
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 
